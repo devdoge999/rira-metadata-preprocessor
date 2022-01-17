d3d328ea1e29218c3dcfaf1045e0fdf1cc7a1df4
devdoge <96713574+devdoge999@users.noreply.github.com>
2022-01-18 05:50:45 +0900

    clear attributes & edit animate

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ const baseUri = "ipfs://NewUriToReplace";
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "https://test",
   creators: [
     {
       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
@@ -20,7 +20,6 @@ const solanaMetadata = {
     },
   ],
 };
-
 
 const layerConfigurations = [
   {
